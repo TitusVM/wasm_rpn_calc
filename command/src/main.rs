553ca44901dd34843d4ac1f8318da60078e18b10
Titus Abele <tvmab@pm.me>
2024-12-06 09:13:49 +0100

    made a longer execution timed main function

--- command/src/main.rs
+++ command/src/main.rs
@@ -6,15 +6,23 @@ use bindings::component::rpn::types::{Engine, Operation};
 
 fn main() {
     let calc = Engine::new();
-    calc.push_operand(1);
-    calc.push_operand(2);
-    calc.push_operation(Operation::Add);
-    let sum: u32 = calc.execute();
 
-    let string = format!("[SUM] {:?}", sum);
+    const N: u32 = 5000; 
+    let mut nth_triangular = 0;
 
-    println!("{}", string);
+    for i in 1..=N {
+        let mut triangular_number = 0;
 
-    // Print to filesystem
-    //fs::write("output.txt", string).unwrap();
+        for j in 1..=i {
+            calc.push_operand(triangular_number);
+            calc.push_operand(j);
+
+            calc.push_operation(Operation::Add);
+
+            triangular_number = calc.execute();
+        }
+
+        nth_triangular = triangular_number;
+    }
+    println!("The {}-th triangular number is {}", N, nth_triangular);
 }
